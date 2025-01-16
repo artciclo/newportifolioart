121817acfeaf7150ac10f9722531ce58f3aa7e30
artciclo <artciclo@gmail.com>
2025-01-15 21:14:09 -0300

    ImageMe updated

--- src/components/ImageMe/index.tsx
+++ src/components/ImageMe/index.tsx
@@ -6,7 +6,7 @@ const ImageMe = () => {
     <ContainerStyled>
       <Image
         src="/assets/img-me.png"
-        alt="Imagem de uma mulher, cabelos lisos e castanhos, sorrindo"
+        alt="Imagem de umm homem, de terno, com olhar sereno e calmo"
         sizes="(max-width: 768px) 50vw, (max-width: 1200px) 40vw, 33vw"
         fill
         placeholder="blur"
